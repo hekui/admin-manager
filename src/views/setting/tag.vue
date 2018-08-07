94f042bd030ed24b4428387522f1792b14e87b3e
zhangchenle <409527716@qq.com>
2018-08-07 17:20:53 +0800

    '增加类型管理、标签管理页面'

--- src/views/setting/tag.vue
+++ src/views/setting/tag.vue
@@ -5,118 +5,7 @@
  */
 <template>
   <div class="content-container">
-    <section class="form">
-      <el-form ref="form" :inline="true" :model="filter">
-        <el-form-item label="发布时间：">
-          <el-date-picker
-            v-model="filter.deliveryTime"
-            type="daterange"
-            align="right"
-            unlink-panels
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            :clearable="true"
-            value-format="yyyy-MM-dd"
-            :picker-options="pickerOptions">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="公众号类型：">
-          <el-cascader
-            expand-trigger="hover"
-            :options="options"
-            v-model="filter.publicType">
-          </el-cascader>
-        </el-form-item>
-        <el-form-item label="公众号名称：">
-          <el-input v-model="filter.publicName" placeholder="请输入名称or微信号"></el-input>
-        </el-form-item>
-        <el-form-item label="文章标题：">
-          <el-input v-model="filter.articleTitle" placeholder="请输入名称orID"></el-input>
-        </el-form-item>
-        <el-form-item label="文章类型：" class="article-type-label">
-          <el-cascader
-            expand-trigger="hover"
-            :options="articleOptions"
-            v-model="filter.articleType">
-          </el-cascader>
-        </el-form-item>
-        <el-form-item>
-          <el-button icon="el-icon-search" @click="submitFilter">搜索</el-button>
-        </el-form-item>
-      </el-form>
-    </section>
-    <section class="table">
-      <el-table
-        :data="listData.list"
-        v-loading="loading"
-        style="width: 100%">
-        <el-table-column
-          type="index"
-          label="序号"
-          width="50">
-        </el-table-column>
-        <el-table-column
-          prop="publicName"
-          label="公众号名称">
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="城市"
-          width="80">
-        </el-table-column>
-        <el-table-column
-          prop="articleTitle"
-          label="文章标题"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="deliveryTime"
-          label="发布时间">
-        </el-table-column>
-        <el-table-column
-          prop="articleType"
-          label="类型">
-        </el-table-column>
-        <el-table-column
-          prop="readingQuantity"
-          label="阅读量">
-        </el-table-column>
-        <el-table-column
-          prop="praisingQuantity"
-          label="点赞量">
-        </el-table-column>
-        <el-table-column
-          prop="wordCount"
-          label="字数">
-        </el-table-column>
-        <el-table-column
-          fixed="right"
-          label="操作"
-          width="100">
-          <template slot-scope="scope">
-            <div>
-              <el-button @click="handleDetail(scope.row)" type="text" size="small">详情</el-button>
-            </div>
-            <div>
-              <el-button @click="handleEdit(scope.row)" type="text" size="small">二次编辑</el-button>
-            </div>
-          </template>
-        </el-table-column>
-      </el-table>
-    </section>
-    <section class="pagination">
-      <el-pagination
-        background
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="page.pageNo"
-        :page-sizes="[10, 20, 30, 50]"
-        :page-size="page.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="listData.totalPage">
-      </el-pagination>
-    </section>
+    标签管理
   </div>
 </template>
 
@@ -124,7 +13,7 @@
 import { mapState } from 'vuex'
 
 export default {
-  name: 'contentlist',
+  name: 'tag',
   data() {
     return {
       loading: false,
