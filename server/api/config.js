84210aac44108f335f8b52d50bc7ea689b179e26
zhangchenle <409527716@qq.com>
2018-08-21 11:11:34 +0800

    '修改图片上传公共参数、城市管理增加状态操作'

--- server/api/config.js
+++ server/api/config.js
@@ -4,6 +4,7 @@ const
   // javaHost = process.env.javaHost || 'http://mock.maifangma.com/', // rap2模拟数据
   // javaHost = process.env.javaHost || 'http://192.168.10.196:8089', // 周波PC
   javaHost = process.env.javaHost || 'http://192.168.10.190:8089', // 张浩亮PC
+  // javaHost = process.env.javaHost || 'http://192.168.10.88:8092', // 何洋PC
   // passportHost = process.env.passportHost || 'http://passport.test1.maifangma.com', // 登陆
   passportHost = process.env.passportHost || 'https://uiap.maifangma.com', // 登陆(正式环境)
   // passportHost = process.env.passportHost || 'http://192.168.10.88:8090' // 登陆（何洋）
