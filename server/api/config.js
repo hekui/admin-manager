2ea46a833eb24585779835e2114b7b3b9fd28ecc
Magic <harryzjm@live.com>
2018-08-21 14:54:23 +0800

    fix

--- server/api/config.js
+++ server/api/config.js
@@ -4,8 +4,8 @@ const
   // javaHost = process.env.javaHost || 'http://???', // 测试环境
   // javaHost = process.env.javaHost || 'http://japi.fangguancha.com', // 生产环境
   // javaHost = process.env.javaHost || 'http://192.168.10.196:8089', // 周波PC
-  // javaHost = process.env.javaHost || 'http://192.168.10.190:8089', // 张浩亮PC
-  javaHost = process.env.javaHost || 'http://192.168.10.88:8092', // 何洋PC
+  javaHost = process.env.javaHost || 'http://192.168.10.190:8092', // 张浩亮PC
+  // javaHost = process.env.javaHost || 'http://192.168.10.88:8092', // 何洋PC
   // passportHost = process.env.passportHost || 'http://passport.test1.maifangma.com', // 登陆
   // passportHost = process.env.passportHost || 'https://uiap.maifangma.com', // 登陆(正式环境)
   passportHost = process.env.passportHost || 'http://192.168.10.88:8090', // 登陆（何洋）
